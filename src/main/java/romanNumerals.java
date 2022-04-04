4c2973f248528af114145d37e4a8e850fd75fcb5
MarcusC67 <93521177+MarcusC67@users.noreply.github.com>
2022-04-04 20:20:33 +0100

    add failing test for 6-8

--- src/main/java/romanNumerals.java
+++ src/main/java/romanNumerals.java
@@ -5,6 +5,7 @@ public class romanNumerals {
     public String integerToRomanNumeral(int inputNum) {
 
         StringBuilder romanStr = new StringBuilder();
+
         while (inputNum >= 1 && inputNum <= 3) {
             romanStr.append("I");
             inputNum = inputNum - 1;
@@ -14,6 +15,17 @@ public class romanNumerals {
             romanStr.append("IV");
             inputNum = inputNum - 4;
         }
+
+        while (inputNum == 5) {
+            romanStr.append("V");
+            inputNum = inputNum - 5;
+        }
+
+        while (inputNum >= 6 && inputNum <= 8) {
+            romanStr.append("V");
+            inputNum = inputNum - 5; // 6=1, 7=2, 8=3
+        }
+
         return romanStr.toString();
     }
 
