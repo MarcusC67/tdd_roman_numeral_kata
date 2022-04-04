7ecd93457818416d0fe17e3d773a616cfecb671a
MarcusC67 <93521177+MarcusC67@users.noreply.github.com>
2022-04-04 19:04:27 +0100

    initial test for TDD intended fail - red

--- src/main/java/romanNumerals.java
+++ src/main/java/romanNumerals.java
@@ -5,9 +5,9 @@ public class romanNumerals {
     public String integerToRomanNumeral(int inputNum) {
 
         StringBuilder romanStr = new StringBuilder();
-        while (inputNum >= 1 && inputNum <= 3) {
+        while (inputNum == 1) {
             romanStr.append("I");
-            inputNum -= 1;
+            inputNum = inputNum - 1;
         }
         return romanStr.toString();
     }
